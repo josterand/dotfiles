135a33f15c259867808342be841ddf23de2d9f44
josterand <josterand@outlook.com>
2024-12-21 00:07:13 +0700

    feat: new & working install script

--- install.sh
+++ install.sh
@@ -11,19 +11,16 @@
 # ***************************************************
 
 # Required variables
-rice_name="dotfiles"
-rice_dir="$HOME/${rice_name}"
-date=$(date "+%Y%m%d-%H%M%S")
-backup_dir="$HOME/Backups/Bspwm/$(whoami)/Backup-${date}"
-dependencies=(
-    "alacritty"
-    "bspwm"
-    "dunst"
-    "sxhkd"
-    "polybar"
-    "rofi"
-)
+title="=================================\nBSPWM rice Installer by Josterand\n================================="
+subtitle="
+This script will install my bspwm rice config files and other necessary files to your system.
+Please backup your existing rice config files and install all the required packages before proceeding.
+For more information, please see https://github.com/josterand/dotfiles.
 
+This repo and it's contents are licensed under the MIT License and come with no warranty of any kind.
+I'm not responsible for any damage caused by this script or the files it installs."
+
+# Function to log messages with different categories
 log() {
     local txt="$1"
     local category="$2"
@@ -44,7 +41,10 @@ log() {
         echo -e "${bold}\033[32m:: ${txt}${reset}" # Green
         ;;
     normal)
-        echo -e "${bold}:: ${txt}${reset}" # Plain
+        echo -e "${bold}:: ${txt}${reset}" # Normal with bold text style
+        ;;
+    plain)
+        echo -e "${txt}" # Plain text without log indicator
         ;;
     *)
         return 1
@@ -52,75 +52,69 @@ log() {
     esac
 }
 
-# Check if dependencies are installed
-pkg_check() {
-    log "Checking dependencies" "info"
-    for dep in "${dependencies[@]}"; do
-        if ! command -v "$dep" &>/dev/null; then
-            log "Error: package $dep not found. Please install it first!" "error"
-            exit 1
-        fi
-        log "$dep is already installed. Good..." "normal"
-    done
-    log "All dependencies are installed!" "success"
-}
-
-rice_backup() {
-    log "Backing up configuration files" "info"
-    mkdir -p $backup_dir
-    for pkg in "${dependencies[@]}"; do
-        if [ ! -d "$HOME/.config/${pkg}" ]; then
-            log "$pkg configuration not exist" "normal"
-        else
-            log "$pkg configuration exist. Backing up the configuration" "normal"
-            mv $HOME/.config/${pkg} $backup_dir
-        fi
-    done
-}
-
-# Function to copy dotfiles
+# Setup dotfiles and configuration files
 rice_setup() {
-    log "Installing the rice..." "info"
-    log "Detecting configuration folders" "info"
+    log "Searching for available configs in dotfiles folder" "normal"
     subfolders=()
-    for subfolder in "$rice_dir/.config"/*/; do
-        [ -d "$subfolder" ] && subfolders+=("$(basename "$subfolder")")
-        log "$subfolder configuration detected!" "normal"
+    for dep in "$(pwd)/.config"/*/; do
+        [ -d "$dep" ] && subfolders+=("$(basename "$dep")")
+        log "$dep config detected!" "normal"
     done
+    sleep 1
 
-    for folder in "${subfolders[@]}"; do
-        log "Copying $folder configurations..." "normal"
-        cp -r "$rice_dir/.config/${folder}" "$HOME/.config/"
+    log "Copying configuration files..." "info"
+    # Copy configuration files
+    for folder_name in "${subfolders[@]}"; do
+        capitalized_folder_name="$(echo "$folder_name" | sed 's/.*/\u&/')"
+        cp -r "$(pwd)/.config/${folder_name}" "$HOME/.config/"
+        log "$capitalized_folder_name config copied!" "normal"
     done
+    sleep 1
+
+    # Copy fonts and scripts
+    cp -r "$(pwd)/.local/share/fonts/" "$HOME/.local/share/fonts"
+    log "Font copied!" "normal"
+
+    cp -r "$(pwd)/.local/bin/" "$HOME/.local/"
+    log "Scripts copied!" "normal"
+    sleep 1
 }
 
-# ==============
-# Main execution
-# ==============
 while true; do
     clear
-    log "The installation process is about to begin. Do you want to continue? (Y/n)" "normal"
-    read -p "> " -n 1 -r
+    log "$title\n$subtitle" "plain"
+    echo
+    read -p ":: Continue? (Y/n) > " -n 1 -r
     echo
 
     response="${REPLY:-Y}"
 
     case $response in
     [Yy])
-        # If the user inputs Y or y
-        pkg_check
-        rice_backup
+        # Create necessary directories
+        log "Creating necessary directories..." "info"
+        log "Creating font folder..." "normal"
+        mkdir -p "$HOME/.local/share/fonts"
+        sleep 1
+
+        log "Creating script folder" "normal"
+        mkdir -p "$HOME/.local/bin"
+        sleep 1
+
+        log "Setting up rice..." "info"
         rice_setup
+        sleep 1
+
         log "Installation complete. Enjoy your rice!" "success"
         break
         ;;
     [Nn])
-        # If the user inputs N or n\
+        # If the user inputs N or n
         log "Operation canceled" "error"
         exit 1
         ;;
     *)
-        # If the user enters an input other than Y, N, or when a key other than 'Enter' is pressed.
+        # If the user enters an input other than Y, N, or when a key other than 'Enter' is pressed
         log "Invalid input. Please enter 'y' or 'n'" "error"
         sleep 3
         ;;
